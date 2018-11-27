ecf1b5587b05681d2f7935956fcc2a9cf5fa0bec
Brian Richter <brianorwhatever@gmail.com>
2018-11-27 15:30:33 -0800

    Test (#4)
    * sorted usings and added db context back in
    
    * Task-3131 - added ispublished parameter to getall endpoint

--- Gcpe.Hub.API/Controllers/MessagesController.cs
+++ Gcpe.Hub.API/Controllers/MessagesController.cs
@@ -1,13 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using Gcpe.Hub.API.ViewModels;
 using Gcpe.Hub.Data.Entity;
-using Gcpe.Hub.API.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Gcpe.Hub.API.ViewModels;
+using Gcpe.Hub.API.Helpers;
 
 namespace Gcpe.Hub.API.Controllers
 {
@@ -30,11 +30,11 @@ namespace Gcpe.Hub.API.Controllers
         [HttpGet]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
-        public IActionResult GetAll()
+        public IActionResult GetAll([FromQuery(Name = "IsPublished")] Boolean IsPublished = true)
         {
             try
             {
-                var messages = dbContext.Message.ToList();
+                var messages = dbContext.Message.Where(m => m.IsPublished == IsPublished).ToList();
                 mapper.Map<List<Message>, List<MessageViewModel>>(messages);
                 return Ok(messages);
             }
@@ -69,11 +69,11 @@ namespace Gcpe.Hub.API.Controllers
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public IActionResult Get(string id)
+        public IActionResult Get(Guid id)
         {
             try
             {
-                var message = dbContext.Message.Find(Guid.Parse(id));
+                var message = dbContext.Message.Find(id);
                 if(message != null)
                 {
                     return Ok(mapper.Map<Message, MessageViewModel>(message));
@@ -92,16 +92,16 @@ namespace Gcpe.Hub.API.Controllers
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public IActionResult Put(string id, MessageViewModel message)
+        public IActionResult Put(Guid id, MessageViewModel message)
         {
             try
             {
-                var oldMessage = dbContext.Message.Find(Guid.Parse(id));
+                var oldMessage = dbContext.Message.Find(id);
                 if(oldMessage == null)
                 {
                     return NotFound($"Message not found with id: {id}");
                 }
-                message.Id = Guid.Parse(id);
+                message.Id = id;
                 dbContext.Entry(oldMessage).CurrentValues.SetValues(mapper.Map<MessageViewModel, Message>(message));
                 dbContext.SaveChanges();
                 return Ok(message);
