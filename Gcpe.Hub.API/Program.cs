e9d0cad43bfe59acc27eea6d39f4679c657adb2d
André Lashley <andre.lashley@gmail.com>
2019-12-16 11:46:40 -0800

    Complete upgrade to dotnetcore 3.0

--- Gcpe.Hub.API/Program.cs
+++ Gcpe.Hub.API/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Gcpe.Hub.API
 {
@@ -12,8 +12,11 @@ namespace Gcpe.Hub.API
             host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
